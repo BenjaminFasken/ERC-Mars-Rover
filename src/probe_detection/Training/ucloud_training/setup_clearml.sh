0c1246d67bf6a1f4785c060072453ba86bb4db99
Emil <ejens22@student.aau.dk>
2025-04-14 09:04:23 +0200

    Update setup_clearml.sh

--- src/probe_detection/Training/ucloud_training/setup_clearml.sh
+++ src/probe_detection/Training/ucloud_training/setup_clearml.sh
@@ -2,5 +2,5 @@
 export CLEARML_API_HOST=https://api.clear.ml
 export CLEARML_WEB_HOST=https://app.clear.ml
 export CLEARML_FILES_HOST=https://files.clear.ml
-export CLEARML_API_ACCESS_KEY="ML028K9PKBBWZMERNUDCDY65X2E94L"
-export CLEARML_API_SECRET_KEY="WQSugnpj240q5X6Vv6oM2XaOs4TiMDwMksX-NICGS-zN0MdpRDVvZWGcqlwcHhlRd88"
+export CLEARML_API_ACCESS_KEY="this is secret lmfao"
+export CLEARML_API_SECRET_KEY="this is secret lmfao"
