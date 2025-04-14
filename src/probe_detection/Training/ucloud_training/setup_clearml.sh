a2207274e9cf97b7f74fc30bcc2d4079360bb946
Emil <ejens22@student.aau.dk>
2025-04-14 09:45:44 +0200

    removing ignored files

--- src/probe_detection/Training/ucloud_training/setup_clearml.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-export CLEARML_API_HOST=https://api.clear.ml
-export CLEARML_WEB_HOST=https://app.clear.ml
-export CLEARML_FILES_HOST=https://files.clear.ml
-export CLEARML_API_ACCESS_KEY="this is secret lmfao"
-export CLEARML_API_SECRET_KEY="this is secret lmfao"
